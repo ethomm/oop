aeda9ade1593f47ee6d2da4973f889a37cf0114a
Eirik Thommessen <eirik@meetprod.no>
2015-02-09 20:35:03 +0100

    Oveningsoppgave 4

---
+++ Oveningsoppgave_4/src/main/Main.java
@@ -0,0 +1,23 @@
+package main;
+
+import gui.MainFrame;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		SwingUtilities.invokeLater(new Runnable() {
+			
+			@Override
+			public void run() {
+				//Starter GUI
+				new MainFrame("¯vningsoppgave 4");
+				
+			}
+		});
+
+	}
+
+}
